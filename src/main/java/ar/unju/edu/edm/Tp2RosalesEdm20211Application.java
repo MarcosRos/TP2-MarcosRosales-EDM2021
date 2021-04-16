9809b1cff26aacc97c283f3d0ca4cb359492cc22
Marcos <Marcos@DESKTOP-KHEO9DL>
2021-04-16 15:43:59 -0300

    TP2

---
+++ src/main/java/ar/unju/edu/edm/Tp2RosalesEdm20211Application.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2RosalesEdm20211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2RosalesEdm20211Application.class, args);
+	}
+
+}
---
+++ src/main/java/ar/unju/edu/edm/controller/ProductoController.java
@@ -0,0 +1,46 @@
+package ar.unju.edu.edm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.unju.edu.edm.model.Producto;
+import ar.unju.edu.edm.service.ProductoService;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	ProductoService iProductoService;
+	
+	@GetMapping("/producto")
+	public String cargarProducto(Model model) {
+		model.addAttribute("unProducto", iProductoService.obtenerProductoNuevo());
+		return("producto");
+	}
+
+	
+	@PostMapping("/producto")
+	public String guardarNuevoProducto(@ModelAttribute("unProducto") Producto nuevoProducto, Model model) {
+		iProductoService.guardarProducto(nuevoProducto);
+		//mostrar el listado de producto luego de la carga de un producto
+		System.out.println(iProductoService.obtenerTodosProductos().get(0).getMarca());
+		model.addAttribute("productos", iProductoService.obtenerTodosProductos());
+		return "resultado";
+	}
+	
+	@GetMapping("/ultimo")
+	public String cargarUltimoProducto(Model model) {
+		model.addAttribute("ultimoProducto", iProductoService.obtenerUltimoProducto());
+		return("mostrar-ultimo");
+	}
+	
+	@GetMapping("/volver")
+	public String cargarNuevoProducto(Model model) {
+		//model.addAttribute("unProducto", iProductoService.obtenerProductoNuevo());
+		return("redirect:/producto");
+	}
+}
